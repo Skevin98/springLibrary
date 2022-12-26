d37d8a268063abe7cc0d6c8a02dc3a3c661f8a93
Skevin98 <sawadogokevin98@gmail.com>
2022-12-26 07:22:07 +0100

    Initial commit

---
+++ src/test/java/ma/ismagi/springlibrary/SpringlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ismagi.springlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
